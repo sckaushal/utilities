070761a643f9271f01259e04e839edd45dce88dc
sckaushal <sckaushal@gmail.com>
2015-09-10 23:54:43 +0800

    Initial commit
    Converts a slightly mangled version of the DBS file export to YNAB

---
+++ dbs-ynab-exporter.ps1
@@ -0,0 +1,17 @@
+﻿param([string] $inputFile, [string] $outfile = "ynabreadyfile.csv")
+
+$output = @();
+
+Import-Csv $inputFile | % {
+    $date = Get-Date $_."Transaction Date" -format dd/MM/yyyy;
+    $output += [pscustomobject] @{
+        Date = $date;
+        Payee = "";
+        Category = "";
+        Memo = $_."Transaction Ref1" + " - " + $_."Transaction Ref2";
+        Outflow = $_."Debit Amount";
+        Inflow = $_."Credit Amount"
+    }
+}
+$output | Export-Csv $outfile -NoTypeInformation
+
